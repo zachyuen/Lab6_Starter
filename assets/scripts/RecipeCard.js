615cb8b4f917885ba51037ccb373ba445558c702
zachyuen <zachyuen007@gmail.com>
2022-11-07 22:42:37 -0800

    Updated RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,7 +8,7 @@ class RecipeCard extends HTMLElement {
 
     // EXPOSE - START (All expose numbers start with A)
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
-    const shadowOpen = this.attachShadow({mode: "open"});
+    this.attachShadow({mode: "open"});
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
     let newArt = document.createElement('article');
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
@@ -91,8 +91,8 @@ class RecipeCard extends HTMLElement {
       font-size: 12px;
     }`;
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
-    shadowOpen.appendChild(newStyle);
-    shadowOpen.appendChild(newArt);
+    this.shadowRoot.appendChild(newStyle);
+    this.shadowRoot.appendChild(newArt);
   }
 
   /**
@@ -121,26 +121,26 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-    let newArticle = this.shadowRoot.querySelector('article');
+    let article = this.shadowRoot.querySelector('article');
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
-    newArticle.innerHTML = `
-    <img src="https://link-to-article.com/recipe-thumbnail.jpg"
-    alt="Recipe Title">
+    article.innerHTML = `
+    <img src="${data.imgSrc}" 
+      alt="${data.imgAlt}">
     <p class="title">
-      <a href="https://link-to-article.com">Title</a>
+      <a href="${data.titleLnk}">${data.titleTxt}</a>
     </p>
-    <p class="organization">The Chef's Organization</p>
+    <p class="organization">${data.organization}</p>
     <div class="rating">
-      <span>5</span>
-      <img src="/assets/images/icons/5-star.svg" alt="5 stars">
-      <span>(500)</span>
+      <span>${data.rating}</span>
+      <img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+      <span>${data.numRatings}</span>
     </div>
-    <time>50 min</time>
+    <time>${data.lengthTime}</time>
     <p class="ingredients">
-      Comma, Separated, List, of, Ingredients
+      ${data.ingredients}
     </p>`;
   }
 }
