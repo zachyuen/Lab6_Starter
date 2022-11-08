0577dcc16605e214124743b1c1d6964db106835f
zachyuen <102705721+zachyuen@users.noreply.github.com>
2022-11-07 23:10:32 -0800

    Update RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,7 +8,7 @@ class RecipeCard extends HTMLElement {
 
     // EXPOSE - START (All expose numbers start with A)
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
-    this.attachShadow({mode: "open"});
+    const newShadow.attachShadow({mode: "open"});
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
     let newArt = document.createElement('article');
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
@@ -91,8 +91,8 @@ class RecipeCard extends HTMLElement {
       font-size: 12px;
     }`;
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
-    this.shadowRoot.appendChild(newStyle);
-    this.shadowRoot.appendChild(newArt);
+    newShadow.appendChild(newStyle);
+    newShadow.appendChild(newArt);
   }
 
   /**
